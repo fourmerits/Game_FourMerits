217c10c91838f9bc95de1e1143bccaa4b00f620d
cosmo <cosmo.mapanje@gmail.com>
2015-02-16 11:23:01 +0800

    Create the main launch

--- app/src/main/java/com/merits/four/gameproject/AboutActivity.java
+++ app/src/main/java/com/merits/four/gameproject/AboutActivity.java
@@ -1,10 +1,15 @@
 package com.merits.four.gameproject;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 /**
  * Created by cosmo on 2/15/15.
  */
-public class AboutActivity extends Activity {
-
+public class AboutActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.about);
+    }
 }
--- app/src/main/java/com/merits/four/gameproject/MainActivity.java
+++ app/src/main/java/com/merits/four/gameproject/MainActivity.java
@@ -1,17 +1,29 @@
 package com.merits.four.gameproject;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends ActionBarActivity {
 
+    Button btn_about;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn_about = (Button)this.findViewById(R.id.btn_aboutgame);
+        btn_about.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AboutActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 
